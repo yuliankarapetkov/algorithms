56e2e166213095998c82cc0dd307f59dedd1b44a
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-08-08 18:28:43 +0300

    1-1-is-unique: another solution from the book

--- 1-1-is-unique/index.ts
+++ 1-1-is-unique/index.ts
@@ -25,7 +25,7 @@ function isUnique(str: string): boolean {
 // Storage issue does not apply in the case of using ASCII chars only.
 function isUniqueAscii(str: string): boolean {
     const asciiCharsLength = 128;
-    
+
     if (str.length > asciiCharsLength) {
         return false;
     }
@@ -40,6 +40,31 @@ function isUniqueAscii(str: string): boolean {
         } 
         
         used[charCode] = true;
+    }
+
+    return true;
+}
+
+// Solution #3:
+// From book: We can reduce our space usage by a factor of eight by using a bit vector. 
+// We will assume, in the below code, that the string only uses the lowercase letters a through z. 
+// This will allow us to use just a single int.
+// Personal note: in the book they are referring to Java's 32-bit ints, and although JS has 64-bit floating point numbers,
+// bitwise operators are performed on 32-bit ints so it's basically the same as in Java.
+// This means that the "array" can only be 32 bits long.
+function isUniqueBitwise(str: string): boolean {
+    let checker = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      const val = str.charCodeAt(i) - 97;
+      const check = 1 << val;
+      console.log(`1 << ${val} = ${check} => 1 << ${val.toString(2)} = ${check.toString(2)}`)
+      if ((checker & check) > 0) {
+        return false;
+      }
+
+      checker = checker | check;
+      console.log(`checker = ${checker} = ${checker.toString(2)}`)
     }
 
     return true;
