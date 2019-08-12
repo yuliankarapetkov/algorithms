5fed7a24f384607197f6521e989760f9df1d1123
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-08-12 13:43:15 +0300

    remove dups no buffer

--- 2-linked-lists/2-1-remove-dups/index.ts
+++ 2-linked-lists/2-1-remove-dups/index.ts
@@ -1,5 +1,8 @@
+// Problem: Write code to remove duplicates from an unsorted linked list. 
+
 import { Node } from '../linked-list';
 
+// Solution #1 (mine):
 export function removeDups(head: Node) {
     let data = { [head.data]: true };
 
@@ -12,6 +15,29 @@ export function removeDups(head: Node) {
             data[n.next.data] = true;
             n = n.next;
         }
+    }
+
+    return head;
+}
+
+// How would you solve this problem if a temporary buffer is not allowed? 
+// Solution #2 (book):
+export function removeDupsNoBuffer(head: Node) {
+
+    let current = head;
+
+    while (current !== null) {
+        let runner = current;
+
+        while (runner.next !== null) {
+            if (runner.next.data === current.data) {
+                runner.next = runner.next.next;
+            } else {
+                runner = runner.next;
+            }
+        }
+
+        current = current.next;
     }
 
     return head;
