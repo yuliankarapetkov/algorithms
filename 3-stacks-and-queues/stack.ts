4194f659302c5c12756a4dec544dc2a0cf04591f
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-08-13 15:13:53 +0300

    stack implementation

---
+++ 3-stacks-and-queues/stack.ts
@@ -0,0 +1,37 @@
+export class StackNode<T> {
+    next: StackNode<T>;
+    constructor(public data: T) {}
+}
+
+export class Stack<T> {
+    private top: StackNode<T>;
+
+    get isEmpty(): boolean {
+        return this.top === null;
+    }
+
+    pop(): T {
+        if (this.top === null) {
+            throw new Error('Empty Stack');
+        }
+
+        const item = this.top.data;
+        this.top = this.top.next;
+
+        return item;
+    }
+
+    push(data: T): void {
+        const item = new StackNode(data);
+        item.next = this.top;
+        this.top = item;
+    }
+
+    peek(): T {
+        if (this.top === null) {
+            throw new Error('Empty Stack');
+        }
+
+        return this.top.data;
+    }
+}
